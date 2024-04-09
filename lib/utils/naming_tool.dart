7d3300ac048b445e2ee7c9cdba16d67dfe5b7316
acsweet <1395723441@qq.com>
2024-04-09 16:53:56 +0800

    取个名字 lilac

---
+++ lib/utils/naming_tool.dart
@@ -0,0 +1,25 @@
+List <String > projectNames =[
+"Chinese rose",//月季
+"violet"//紫罗兰；
+"cotton tree" //木棉
+"lilac"//紫丁香
+ "lily"//百合
+"wall flower" //紫罗兰
+ "peach"//桃花
+ wisteria；//紫藤
+ tree peony；//牡丹
+ peony；//芍药
+ common camellia；//茶花
+ cape jasmine //栀子花
+cockscomb //鸡冠花；
+honeysuckle //金银花；
+chrysanthemum //菊花；
+carnation // 康乃馨；
+orchid    //兰花；
+jasmine //茉莉花；
+daffodil//水仙花；
+peony//牡丹；
+begonia//秋海棠；
+cactus//仙人掌；
+poppy//罂粟；
+];
