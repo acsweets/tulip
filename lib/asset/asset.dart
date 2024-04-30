e6606d859f53809cae8d46b1a02ed8a7aa9cc6be
acsweet <1395723441@qq.com>
2024-04-30 17:06:09 +0800

    添加

---
+++ lib/asset/asset.dart
@@ -0,0 +1,13 @@
+class Assets {
+
+  ///图片
+  static const bg_1 = "assets/images/bg1.jpg";
+  static const icLove = "assets/images/bg1.jpg";
+  ///数据
+
+
+  ///icon
+  static const articleData = "assets/data/article.json";
+}
+
+//照片故事板块
